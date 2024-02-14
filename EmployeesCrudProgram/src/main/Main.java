7b0981214d9c068c0f3de753f9aa0c2667f23ad0
Marvel Collin <marvelcollin7@gmail.com>
2024-02-14 14:04:21 +0700

    feat: add insert function, add codeName builder MM-XXXX

--- EmployeesCrudProgram/src/main/Main.java
+++ EmployeesCrudProgram/src/main/Main.java
@@ -1,12 +1,14 @@
 package main;
 
 
+import java.util.Random;
 import java.util.Scanner;
 
 import models.Employees;
 
 public class Main {
 	private Scanner s = new Scanner(System.in);
+	private Random random = new Random();
 	
 	private void clr() {
 		for(int i = 0; i < 40; i++) {
@@ -22,11 +24,49 @@ public class Main {
 		String position = null;
 		double salary = 0;
 		
-		do {
-			System.out.print("Input Employees Name [min : 3]> ");
-			name = s.nextLine();
-		} while (name.length() < 3);
+//		- Kode Karyawan -> MM-XXXX (M -> Alphabeth; X -> Number) -> Random -> Unique
+//		- Gaji -> Manager -> 8jt | Supervisor -> 6jt | Admin -> 4jt
 		
+//		do {
+//			System.out.print("Input Employees Name [min : 3]> ");
+//			name = s.nextLine();
+//		} while (name.length() < 3);
+//		
+//		do {
+//			System.out.print("Input " + name + "'s Gender [Male | Female] > ");
+//			gender = s.nextLine();
+//		} while(!gender.equals("Male") && !gender.equals("Female"));
+//		// jika gender bukan Male atau Female maka akan ngulang
+//		
+//		do {
+//			System.out.print("Input " + name + "'s Position [Manager | Supervisor | Programmer] > ");
+//			position = s.nextLine();
+//		} while (!position.equals("Manager") && !position.equals("Supervisor") && !position.equals("Programmer"));
+		
+		String alphabeth = "QWERTYUIOPASDFGHJKLZXCVBNM";
+		
+		StringBuilder codeNameBuilder = new StringBuilder();
+		// supaya bisa .append()
+		
+		
+		//MM-XXXX -> size -> 7 -> Alphabeth(2) -(1) Number(4)
+		for(int i = 0; i < 7; i++) {
+			//MM
+			if(i < 2) {
+				char randomCode = alphabeth.charAt(random.nextInt(alphabeth.length()));
+				codeNameBuilder.append(randomCode);
+			} else if(i == 2) {
+				codeNameBuilder.append("-");
+			} else {
+				codeNameBuilder.append(random.nextInt(9));
+			}
+		}
+		
+		System.out.println(codeNameBuilder);
+		
+		
+		
+
 	}
 	
 	private void menu() {
@@ -62,7 +102,9 @@ public class Main {
 	}
 	
 	public Main() {
-		menu();
+//		menu();
+		insert();
+		
 	}
 	
 	public static void main(String[] args) {
