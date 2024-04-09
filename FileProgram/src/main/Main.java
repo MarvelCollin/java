3e9459569f181bbd95f4e3499ce344215687433e
Marvel Collin <marvelcollin7@gmail.com>
2024-04-09 19:39:13 +0700

    feat: add models

--- FileProgram/src/main/Main.java
+++ FileProgram/src/main/Main.java
@@ -3,23 +3,36 @@ package main;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+
+import models.Data;
+
 
 public class Main {
 
 	public Main() {
-		// letakin file txt n
+		List<Data> datas = new ArrayList<>();
+		
+		// letakin file txt ke projectnya bukan di dalam src
 		String filePath = "data.txt";
 		
 		try {
             List<String> lines = Files.readAllLines(Paths.get(filePath));
             
-            for (String line : lines) {
-                System.out.println(line);
+            for(String line : lines) {
+            	String[] parts = line.split("#", 2);
+            	
+            	Data data = new Data(parts[0], Integer.parseInt(parts[1]));
+            	datas.add(data);
             }
-        } catch (IOException e) {
+		} catch (IOException e) {
             System.out.println("Failed");
         }
+		
+		for(Data data : datas) {
+			System.out.println("Name : " + data.getName() + " Balance : " + data.getBalance());
+		}
 	}
 
 	public static void main(String[] args) {
---
+++ FileProgram/src/models/Data.java
@@ -0,0 +1,31 @@
+	package models;
+	
+	public class Data {
+		private String name;
+		private int balance;
+		
+		public Data(String name, int balance) {
+			super();
+			this.name = name;
+			this.balance = balance;
+		}
+	
+		public String getName() {
+			return name;
+		}
+	
+		public void setName(String name) {
+			this.name = name;
+		}
+	
+		public int getBalance() {
+			return balance;
+		}
+	
+		public void setBalance(int balance) {
+			this.balance = balance;
+		}
+		
+	
+	}
+		
